bab19ede4283c382adcdfb5306af2ec86029cfc1
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-10-15 17:20:45 -0400

    finished ClearController.java

--- src/interface_adapter/clear_users/ClearController.java
+++ src/interface_adapter/clear_users/ClearController.java
@@ -1,5 +1,18 @@
 package interface_adapter.clear_users;
 
-// TODO Complete me
+import com.sun.org.apache.xerces.internal.xs.StringList;
+import use_case.clear_users.*;
+
 public class ClearController {
+
+    final ClearInputBoundary clearUseCaseInteractor;
+    public ClearController(ClearInputBoundary clearUseCaseInteractor){
+        this.clearUseCaseInteractor = clearUseCaseInteractor;
+    }
+
+    public void execute(StringList usernames) {
+        ClearInputData clearInputData = new ClearInputData(usernames);
+
+        clearUseCaseInteractor.execute(clearInputData);
+    }
 }
