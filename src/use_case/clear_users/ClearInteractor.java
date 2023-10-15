0d7ab81373c51da0d1466ad2e25a93b8dd7e8d78
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-10-15 17:55:50 -0400

    finished ClearInteractor.java

--- src/use_case/clear_users/ClearInteractor.java
+++ src/use_case/clear_users/ClearInteractor.java
@@ -1,6 +1,31 @@
 package use_case.clear_users;
 
-// TODO Complete me
+import com.sun.org.apache.xerces.internal.xs.StringList;
+import entity.User;
 
-public class ClearInteractor {
+public class ClearInteractor implements ClearInputBoundary {
+    final ClearUserDataAccessInterface userDataAccessObject;
+    final ClearOutputBoundary clearPresenter;
+
+    public ClearInteractor(ClearUserDataAccessInterface userDataAccessObject,
+                           ClearOutputBoundary clearOutputBoundary){
+        this.userDataAccessObject = userDataAccessObject;
+        this.clearPresenter = clearOutputBoundary;
+    }
+
+    @Override
+    public void execute(ClearInputData clearInputData) {
+        StringList usernames = clearInputData.getUsernames();
+        StringList usernamesSuccessfullyDeleted = null;
+        for(Object username : usernames){
+            if(!userDataAccessObject.existsByName((String)username)){
+                clearPresenter.prepareFailedView(username + ": Account does not exist.");
+            } else {
+                userDataAccessObject.delete((String) username);
+                usernamesSuccessfullyDeleted.add(username);
+            }
+        }
+        ClearOutputData clearOutputData = new ClearOutputData(usernamesSuccessfullyDeleted, false);
+        clearPresenter.prepareSuccessView(clearOutputData);
+    }
 }
