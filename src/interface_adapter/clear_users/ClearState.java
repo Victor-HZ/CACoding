40c95aa1b649e1d87981a72ba538ac396a3982cb
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-10-15 17:34:22 -0400

    finished ClearState.java

--- src/interface_adapter/clear_users/ClearState.java
+++ src/interface_adapter/clear_users/ClearState.java
@@ -1,6 +1,29 @@
 package interface_adapter.clear_users;
 
-// TODO Complete me
+import com.sun.org.apache.xerces.internal.xs.StringList;
 
 public class ClearState {
+    private StringList usernames;
+    private String usernameError = null;
+
+    public ClearState(ClearState copy){
+        usernames = copy.usernames;
+        usernameError = copy.usernameError;
+    }
+
+    public ClearState(){}
+
+    public StringList getUsernames(){return usernames;}
+
+    public String getUsernameError() {
+        return usernameError;
+    }
+
+    public void setUsernames(StringList usernames){
+        this.usernames = usernames;
+    }
+
+    public void setUsernameError(String usernameError) {
+        this.usernameError = usernameError;
+    }
 }
