a3a98d2b0ab37a8473a1ffe5c94c87c4a6d963a8
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-10-15 17:38:55 -0400

    finished ClearPresenter.java

--- src/interface_adapter/clear_users/ClearPresenter.java
+++ src/interface_adapter/clear_users/ClearPresenter.java
@@ -1,6 +1,35 @@
 package interface_adapter.clear_users;
 
-// TODO Complete me
+import interface_adapter.ViewManagerModel;
+import use_case.clear_users.ClearOutputBoundary;
+import use_case.clear_users.ClearOutputData;
 
-public class ClearPresenter {
+public class ClearPresenter implements ClearOutputBoundary {
+
+    private final ClearViewModel clearViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public ClearPresenter(ClearViewModel clearViewModel,
+                          ViewManagerModel viewManagerModel){
+        this.clearViewModel = clearViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    @Override
+    public void prepareSuccessView(ClearOutputData response) {
+        ClearState clearState = clearViewModel.getState();
+        clearState.setUsernames(response.getUsernames());
+        this.clearViewModel.setState(clearState);
+        this.clearViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setActiveView(clearViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailedView(String error) {
+        ClearState clearState = clearViewModel.getState();
+        clearState.setUsernameError(error);
+        clearViewModel.firePropertyChanged();
+    }
 }
