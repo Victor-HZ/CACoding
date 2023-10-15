a4e3fe2ea28583dc200dd7d6758a163bf973d5bf
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-10-15 17:07:09 -0400

    finished ClearInputData.java

--- src/use_case/clear_users/ClearInputData.java
+++ src/use_case/clear_users/ClearInputData.java
@@ -1,6 +1,17 @@
 package use_case.clear_users;
 
-// TODO Complete me
+import com.sun.org.apache.xerces.internal.xs.StringList;
 
 public class ClearInputData {
+
+    final private StringList usernames;
+
+    public ClearInputData(StringList usernames){
+        this.usernames = usernames;
+    }
+
+    StringList getUsernames(){
+        return usernames;
+    }
+
 }
