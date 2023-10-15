6106818931a9b24f0727afaf62bc5fcfbddbfa6e
Victor-HZ <144486877+Victor-HZ@users.noreply.github.com>
2023-10-15 17:00:58 -0400

    finished ClearOutPutData.java

--- src/use_case/clear_users/ClearOutputData.java
+++ src/use_case/clear_users/ClearOutputData.java
@@ -1,6 +1,18 @@
 package use_case.clear_users;
 
-// TODO Complete me
+import com.sun.org.apache.xerces.internal.xs.StringList;
+import entity.User;
 
 public class ClearOutputData {
+    private final StringList usernames;
+    private boolean useCaseFaild;
+
+    public ClearOutputData(StringList usernames, boolean useCaseFaild){
+        this.usernames = usernames;
+        this.useCaseFaild = useCaseFaild;
+    }
+
+    public StringList getUsernames() {
+        return usernames;
+    }
 }
